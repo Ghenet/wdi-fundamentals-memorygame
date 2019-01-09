6913053d2bb485d4372eaf241a470f9d6cc342e3
Ghenet Kibrom <ghenetkibrom@Ghenets-MacBook-Pro.local>
2019-01-08 19:45:37 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,21 @@
-console.log('up and running!');
+//console.log('up and running!');
 
-var  cardOne = 'queen';
-var  cardTwo = 'king';
-var  cardThree = 'queen';
-var  cardFour = 'king';
+//console.log('User flipped ' + cardTwo);
 
-console.log('User flipped ' + cardTwo);
+ var cards =["queen" , "queen" ,"king" , "king"]
+ var cardsInPlay = [];
+
+ var cardOne = cards[0];
+ cardsInPlay.push(cardOne);
+ console.log('User flipped ' + cardsInPlay[0]);
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo)
+console.log('User flipped ' + cardsInPlay[1]);
+if(cardsInPlay.length === 2){
+  if(cardsInPlay[0]===cardsInPlay[1]){
+    alert('You found a match!');
+  }else{
+    alert("Sorry, Try again.")
+  }
+}
