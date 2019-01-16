25a202ff5f11b2e42cf39edcc07128d35b7dfba5
Ghenet Kibrom <ghenetkibrom@Ghenets-MacBook-Pro.local>
2019-01-16 14:19:40 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,12 @@
 //console.log('up and running!');
 
 //console.log('User flipped ' + cardTwo);
+//"queen" , "queen" ,"king" , "king"
 
- var cards =["queen" , "queen" ,"king" , "king"]
+ var cards =[{rank: "queen" , suit: "hearts" , cardImage:"images/queen-of-hearts.png" } ,
+             {rank: "queen" , suit: "diamonds" , cardImage: "images/queen-of-diamonds.png"},
+             {rank: "king"  , suit: "hearts" , cardImage: "images/king-of-hearts.png"},
+             {rank: "king"  , suit: "diamonds" , cardImage: "images/king-of-diamonds.png"}]
  var cardsInPlay = [];
 
  var checkForMatch = function(){
@@ -27,8 +31,10 @@
 
   if(cardsInPlay.length === 2){
      checkForMatch();
-    console.log( "User flipped " + card[cardId]);
-    cardsInPlay.push(cards[cardId]);
+    console.log( "User flipped " + cards[cardId].rank);
+    cardsInPlay.push(cards[cardId].rank);
+    console.log(cards[cardId].cardImage);
+    console.log(cards[cardId].suit);
  }
 
 }
